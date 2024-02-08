0ee4fc82f8d6fd106f3ce44216e59f4e0532adc2
PrathamManabasannanavar <128047712+PrathamManabasannanavar@users.noreply.github.com>
2024-02-08 20:19:15 +0530

    Create intro.java

---
+++ Java Basics/intro.java
@@ -0,0 +1,23 @@
+class Bye
+{
+    private int data;
+    public int sum(int key)
+    {
+        data += key;
+        System.out.println(data);
+        return 0;
+    }
+     Bye(int no) //Using constructors
+    {
+        data = no; //setting the no
+    }
+}
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        Bye hello = new Bye(20); //creating a object
+        hello.sum(10);
+    }
+}
+
