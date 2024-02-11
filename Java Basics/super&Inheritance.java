026ba911bb90cb6444961809c7f913f55aadde3c
PrathamManabasannanavar <128047712+PrathamManabasannanavar@users.noreply.github.com>
2024-02-11 20:19:19 +0530

    Add files via upload

---
+++ Java Basics/super&Inheritance.java
@@ -0,0 +1,37 @@
+//A simple Java program to demonstrate super method in Constructor
+class Student
+{
+	  String name;
+	  int rollNo;
+	  
+	  static //This block is initiated once
+	  {
+	  	   System.out.println("in Static");
+	  }
+	  public Student()
+	 {
+	       System.out.println("in Student constructor");
+	  }
+}
+
+class College extends Student
+{
+	int id;
+	String dept;
+	public College()
+	{
+	       super(); //no need to write because automatically executed	     
+	       System.out.println("in College constructor");		
+	}
+	
+}
+
+public class Main {
+	public static void main(String[] args) {
+		College c = new College();
+		c.name = "Rohit";
+	    c.id = 23;
+	    c.dept = "CSE";
+		System.out.println("id = " + c.id + " Name = " + c.name);
+	}
+}
