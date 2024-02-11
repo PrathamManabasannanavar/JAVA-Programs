bfaab7c7973c93aa30f0a8bdd34d751b1b482873
PrathamManabasannanavar <128047712+PrathamManabasannanavar@users.noreply.github.com>
2024-02-11 12:33:36 +0530

    Update staticVariable.java

--- Java Basics/staticVariable.java
+++ Java Basics/staticVariable.java
@@ -1,43 +1,46 @@
 //A simple Java program to Demonstrate Static variable and Constructor
+
 import java.util.*;
 
 //1st ---> class load
 //2nd Object Instantation (creation of object)
 class Student
 {
-	String name;
-	static int rollNo;
-	
-	void setValue(String Name, int roll)
-	{
-		name = Name;
-		rollNo = roll;
-        }
-   static
-   {
-   	rollNo = 24;
-   	System.out.println("In Static");
-   }
-   void display()
-   {
-   	System.out.println("Name " + name + " Roll " + rollNo);
-   }
-   Student()
-   {
-   	name = "harsh";
-   	//rollNo = 69;
-   }
+  String name;
+  static int rollNo;
+
+  void setValue (String Name, int roll)
+  {
+	name = Name;
+	rollNo = roll;
+  }
+  static
+  {
+	rollNo = 24;
+	System.out.println ("In Static");
+  }
+  void display ()
+  {
+	System.out.println ("Name " + name + " Roll " + rollNo);
+  }
+  Student ()
+  {
+	name = "harsh";
+	//rollNo = 69;
+  }
 }
 
-public class Main {
-	public static void main(String[] args) {
-		 System.out.println("hi");
-		 //Student s = new Student()
-		 //s.display();
-		 Student s1[] = new Student[2];
-		 s1[0] = new Student();
-		 s1[1] = new Student();
-		 s1[0].display();
-		 s1[1].display();		 		 
-	}
+public class Main
+{
+  public static void main (String[]args)
+  {
+	System.out.println ("hi");
+	//Student s = new Student()
+	//s.display();
+	Student s1[] = new Student[2];
+	  s1[0] = new Student ();
+	  s1[1] = new Student ();
+	  s1[0].display ();
+	  s1[1].display ();
+  }
 }
