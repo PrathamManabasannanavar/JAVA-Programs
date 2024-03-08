d16c3c84858aa22d16d0766d1931040fa488aa30
PrathamManabasannanavar <128047712+PrathamManabasannanavar@users.noreply.github.com>
2024-03-08 17:45:49 +0530

    Create FinalKeyword.java

---
+++ Java Basics/FinalKeyword.java
@@ -0,0 +1,26 @@
+//Java Program to demonstrate final keyword 
+
+//final keyword:
+//for variables  --> const
+//for methods ---> cannot override in subclasses
+//for classes --> cannot inherit
+
+
+class A {
+	final int a = 5;
+	public final void hello() {
+		System.out.println("hello");
+	}
+}
+
+class B extends A{
+	public void hello() {} //throws error}
+}
+
+public class FinalKeyword {
+	public static void main(String[] args) {
+		B b = new B();
+		System.out.println(b.a);
+		b.hello();
+	}
+}
