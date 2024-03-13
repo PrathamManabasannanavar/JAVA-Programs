8892ee30ef0bafa0cd7a2fcb875b88c3cdf0276c
PrathamManabasannanavar <128047712+PrathamManabasannanavar@users.noreply.github.com>
2024-03-13 18:33:57 +0530

    Create Exception2.java

---
+++ Java Basics/Exception2.java
@@ -0,0 +1,28 @@
+//A simple Java program to demonstrate the User defined Exceptions
+import java.util.*;
+
+class MyException  extends Exception{
+  MyException() {
+    super("hi in MyException");
+  }
+  MyException(String str) {
+    
+  }
+}
+public class Main {
+    public static void main(String[] args) {
+      System.out.println("Hello, World!");
+      int a = 4;
+      int b = 0;
+      try {
+        a /= b;
+      }
+      catch(ArithmeticException e) {
+       MyException my = new MyException();
+       System.out.println(my.getMessage());
+       System.out.println("in block");
+      }
+      System.out.println("After block");
+      
+  }
+}
