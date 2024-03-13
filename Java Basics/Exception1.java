ba4dd024532ea3c4af03c0c7d304f737e0826fbc
PrathamManabasannanavar <128047712+PrathamManabasannanavar@users.noreply.github.com>
2024-03-13 17:26:22 +0530

    Create Exception1.java

---
+++ Java Basics/Exception1.java
@@ -0,0 +1,23 @@
+//A simple Java program ro demonstrate the Exceptions
+import java.util.*;
+
+public class Exception1 {
+    public static void main(String[] args) {
+      System.out.println("Hello, World!");
+      int a = 4;
+      int b = 0;
+      try {
+        System.out.println(a/b);
+      }
+      catch(ArrayIndexOutOfBoundsException e){
+        System.out.println("in ArrayIndexOutOfBounds excep");
+      }
+      catch(ArithmeticException e){
+        System.out.println("in Arithmetic excep");
+        throw new ArithmeticException("Arithmetic exception occurred");
+      }
+      catch(Exception e) {
+        System.out.println("in catch");
+      }
+  }
+}
