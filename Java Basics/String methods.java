b6f9720eda4851b2d82c08ade1f71686d923d6bb
PrathamManabasannanavar <128047712+PrathamManabasannanavar@users.noreply.github.com>
2024-03-15 18:08:25 +0530

    Create String methods.java

---
+++ Java Basics/String methods.java
@@ -0,0 +1,15 @@
+//A simple Java program to demonstrate the Immutable Strings in Java
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      System.out.println("Hello, World!");
+      String str1 = "  hi255662k";
+      String str2 = "bye";
+      String str3[] = str1.split("i");
+      System.out.println(str1.concat(str2));
+      System.out.println(str1.trim());
+      System.out.println(str1.substring(2, 5));
+      System.out.println(str3[1]);
+  }
+}
