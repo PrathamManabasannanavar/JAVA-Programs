b4728de9d06836c8fc108afd6942a10425db78b6
PrathamManabasannanavar <128047712+PrathamManabasannanavar@users.noreply.github.com>
2024-04-08 16:47:01 +0530

    Create CustomExceptions1.java

---
+++ Java Basics/CustomExceptions.java
@@ -0,0 +1,33 @@
+//A simple Java program to demonstrate the Custom Exceptions in Java
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      int n = 1000;
+      try{
+          throw new ErrorMessage(n);
+      }
+      catch(ErrorMessage em){
+        System.out.println(em);
+      }
+  }
+}
+
+class ErrorMessage extends Exception {
+    int n;
+    ErrorMessage(int n){
+      this.n = n;
+    }
+    public String toString(){
+      if(n < 500){
+        return "too less temperature";
+      }
+      else if(n >= 500){
+        return "too high temperature";
+      }
+      else{
+        return "Normal temperature";
+      }
+      
+    }
+}
