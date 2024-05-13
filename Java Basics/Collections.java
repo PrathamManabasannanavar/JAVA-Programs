62435be31f08987274111a7b6f16c257390942b1
PrathamManabasannanavar <128047712+PrathamManabasannanavar@users.noreply.github.com>
2024-05-13 21:13:22 +0530

    Create Collections.java

---
+++ Java Basics/Collections.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      List<Integer> c1 = new ArrayList<>();
+      System.out.println(c1);
+      c1.add(3);
+      c1.addAll(Arrays.asList(4,5,6));
+      c1.remove(1);
+      System.out.println(c1.contains(8));
+      System.out.println(c1.get(2));
+      System.out.println(c1);
+      
+      Map<Integer, String> mp = new HashMap<>();
+      mp.put(5,"hi");
+      mp.put(6,"bye");
+      mp.put(7, "google");
+      mp.put(8,"hi");
+      System.out.println(mp);
+     // mp.remove(5);
+      System.out.println(mp.keySet());
+      System.out.println(mp.values());
+      System.out.println(mp.get(6));
+      System.out.println(mp.entrySet());
+  }
+}
