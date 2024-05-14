5004b31ad18936e271cae7d8016ac18f327d5dff
PrathamManabasannanavar <128047712+PrathamManabasannanavar@users.noreply.github.com>
2024-05-14 17:42:52 +0530

    Create Colection basics.java

---
+++ Java Basics/Colection basics.java
@@ -0,0 +1,16 @@
+//A simple Java program to find differentiate Collection and Collections
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      System.out.println("Hello, World!");
+      Collection<String> c1 = new ArrayList<String>();
+      c1.add("bye");
+      Collections.addAll(c1, "hi"); 
+      /*
+         Collection -> interface
+         Collections -> class
+      */
+      System.out.println(c1);
+  }
+}
